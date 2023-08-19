63d81c6d601d83b1da015d23dfcd016d5fff193c
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 12:12:10 +0530

    firstcoomit

--- variable5.sh
+++ variable5.sh
@@ -7,6 +7,10 @@ NUMBER2=$2
 # I need to run a command inside shell to add these 2 numbers
 SUM=$((NUMBER1+NUMBER2))
 ADD=$((NUMBER1-NUMBER2))
+MUL=$((NUMBER1*NUMBER2))
+DIV=$((NUMBER1/NUMBER2))
 
 echo "Addition of 2 numbers is: $SUM"
 echo "Subtraction of 2 num is: $ADD"
+echo "Multiaction of 2 num is: $MUL"
+echo "division of 2 num is : $DIV"
