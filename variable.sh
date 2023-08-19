614f7b9633801c93c72edb716264187fd239542f
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 11:23:50 +0530

    firstcoomit

---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hello am very intersnlgly learining the devops with aws please coperate me"
+PERSON1= Maruthi
+PERSON2= Maruthi2
+
+echo "$PERSON1: Hey $PERSON2 how are you man"
+echo "$PERSON2: Hello my $PERSON1 am extermly fine"
