b9694ac01b2d8220ead374995aea74cad0c3d562
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 13:46:44 +0530

    arrayscript

--- array.sh
+++ array.sh
@@ -4,3 +4,5 @@ PERSONS=("maruthi" "raju" "mahi" "naveen")
 echo "first person name is: ${PERSONS[0]}"
 echo "All persons name: ${PERSONS[@]}"
 echo "the last person name: ${PERSONS[3]}"
+
+# echo samepleadia flahfasflfalfasl flakfas
