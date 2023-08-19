83055d732cef4f719a6df3919fc5c5369478ed0a
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 12:06:21 +0530

    firstcoomit

--- variable5.sh
+++ variable5.sh
@@ -6,5 +6,7 @@ NUMBER1=$1
 NUMBER2=$2
 # I need to run a command inside shell to add these 2 numbers
 SUM=$((NUMBER1+NUMBER2))
+ADD=$((NUMBER1-NUMBER2))
 
 echo "Addition of 2 numbers is: $SUM"
+echo "Subtraction of 2 num is:
