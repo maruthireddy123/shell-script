0a85977f2ab29222210f9ba1ddd9facf1821f7c7
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 17:01:29 +0530

    mysqal

--- mysql.sh
+++ mysql.sh
@@ -2,30 +2,20 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+VALIDATE() {
+if [ $1 -ne 0 ]
 then 
    echo "please intall the user under root user"
    exit 1
 #else 
  # echo "you are root user we are install install the git"
   fi
+}
+   yum install mysql -y
 
-   yum install git -y
-
-if [ $? -ne 0 ]
-then 
-   echo "Installation of mysql is errro"
-   exit 1
-else
-   echo "Installation of mysql is success"
-   fi
+VALIDATE $? 
 
 yum install postfix -y 
 
-if [ $? -ne 0 ]
-then 
-   echo "Installation of postfix is error"
-   exit 1 
-else
-   echo "installation of postfix is success"
-fi
+VALIDATE $?
+
