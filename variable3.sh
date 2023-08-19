2cb6edcf84703d750bc79a3e9305970f913c4240
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 11:50:52 +0530

    firstcoomit

--- variable3.sh
+++ variable3.sh
@@ -6,7 +6,3 @@ MIN=$((NUMBER1-NUMBER2))
 MAX=$((NUMBER1*NUMBER2))
 DIV=$((NUMBER1%NUMBER2))
 echo "print the add value:$SUM"
-echo "print the min value:$MIN"
-echo "print the max value:$MAX"
-echo "print the div value:$DIV"
-
