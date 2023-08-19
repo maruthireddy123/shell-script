53ff47a778eb5dbfab9ea555ca75ab482fcb9fb8
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 13:57:13 +0530

    arrayscript

---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter the your name:"
+read USERNAME
+echo "username entered is:$USERNAME"
+echo "pelase enter your password:"
+read -s PASSWORD
+echo "password entered is: $PASSWORD"
