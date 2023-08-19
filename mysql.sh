3c3b545af0cb5a1ec9fce8f601a6637a5ce699da
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 16:26:46 +0530

    mysqal

--- mysql.sh
+++ mysql.sh
@@ -5,10 +5,16 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "please intall the user under root user"
-else 
-  echo "you are root user we are install install the git"
-
-  yum install git -y
-
+   exit 1
+#else 
+ # echo "you are root user we are install install the git"
   fi
-  
+
+   yum install git -y
+
+if [ $? -ne 0 ]
+then 
+   echo "Installation of mysql is errro"
+else
+   echo "Installation of mysql is success"
+   fi
