2204130cc21b056bfcfca85785a3449a7e73a078
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-19 14:47:27 +0530

    devops.sh from maruthi l2 team

---
+++ conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$!
+if [ $NUMBER -gt 10 ] 
+then 
+   echo "the $NUMBER is greatert than are eqal to zero"
+else
+    echo "the $NUMBER is not greater than eqal to zero"
+fi
+
