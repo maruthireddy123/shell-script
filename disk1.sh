e1052f3ced02361067e57b7d170a072a42d3e8f8
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-24 12:51:09 +0530

    diskuage alert

--- disk1.sh
+++ disk1.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 LOG_FILE_DIR=/tmp
-DATE=$(date +%F)
+DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOG_FILE=$LOG_FILE_DIR/$SCRIPT_NAME-$DATE.log
 
-DISK_USGAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
-DISK_USGAGE_THRESHOLD=1
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem') &>>$LOG_FILE
+DISK_USAGE_THRESHOLD=1
 message=""
 
-while IFS= read line
-do 
-usage=$(echo $line | awk '{print $6}' | cut -d % f1)
-partition=$(echo $line | awk '{print $1}')
-if [ $usage -gt $DISK_USAGE_THRESHOLD ];
-    then
-        message+="HIGH DISK USAGE on $partition: $usage\n"
+#IFS means internal field seperator is space:
+ while IFS= read line 
+ do 
+  usage=$(echo $line | awk '{print $6}' | cut -d % -f1) &>>$LOG_FILE
+  partition=$(echo $line | awk '{print $1}')
+  if [ $usage -gt $DISK_USAGE_THRESHOLD ];
+  then 
+    message+="HIGH DISK USAGE on $partition: $usage\n" &>>$LOG_FILE
     fi
-done <<< $DISK_USAGE
-echo -e "message: $message"
+done <<< $DISK_USAGE 
+
+echo -e "message: $message" 
