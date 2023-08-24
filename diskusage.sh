1fa5195dab1e08cca3e1e5d19ed3dbadcf973cf4
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-24 12:14:41 +0530

    diskuage alert

---
+++ diskusage.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+LOG_FILE_DIR=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOG_FILE=$LOG_FILE_DIR/$SCRIPT_NAME-$DATE.log
+R="\e[31"
+G="\e[32"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
+message=""
+
+#IFS means internal field seperator is space:
+ while IFS= read line 
+ do 
+  usage=$(echo $line | awk '{print $6}' | cut -d % f1)
+  partition=$(echo $line | awk '{print $1}')
+  if [ $USAGE -gt $DISK_USAGE_THRESHOLD ];
+  then 
+    message+="HIGH DISK USAGE on $partition: $usage\n"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "message: $message"
