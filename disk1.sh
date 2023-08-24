e9c94b2166de0a56b536ffa0b60f461d11097595
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-24 12:36:24 +0530

    diskuage alert

---
+++ disk1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+LOG_FILE_DIR=/tmp
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOG_FILE_LOC=$(LOG_FILE_DIR/$SCRIPT_NAME/$DATE.log
+
+DISK_USGAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USGAGE_THRESHOLD=1
+message=""
+
+while IFS read line
+do 
+usage=$(echo $line | awk '{print $6}' | cut -d % f1)
+partition=$ (echo $line | awk '{print1}')
+if [ $usage -gt $DISK_USGAGE_THRESHOLD ];
+thne 
+message+= "HIGH DISK USGAE for $partition: $usage\n"
+fi 
+done
+echo -e "message: $message"
