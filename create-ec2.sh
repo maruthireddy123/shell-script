a2b3f8de127cae12a608557be57f124604bffe7f
maruthireddy123 <sunrisershyd.srh123@gmail.com>
2023-08-27 10:53:34 +0530

    creating a instances with names

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,20 +15,5 @@ INSTANCE_TYPE="t2.micro"
 fi
 echo "creating a $i instance"
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
-
-        aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
-    {
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
-done
+IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
